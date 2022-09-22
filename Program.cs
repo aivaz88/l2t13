d7739b439d9944284ec6aafd1342092442ec4d2a
aivaz88 <aivaz1988@mail.ru>
2022-09-23 00:52:13 +0500

    comment

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+
+Console.Write("Введите положительное число: ");
+int num = int.Parse(Console.ReadLine());
+int count = 0;
+int x = num;
+
+if (num < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    while(x != 0)
+    {
+        x = x / 10;
+        count++;
+    }
+    while(count > 3)
+    {
+        count--;
+        num = num / 10;
+    }
+    Console.WriteLine(num % 10);
+}
